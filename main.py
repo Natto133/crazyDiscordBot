725f1014dba053939fba6c13a7753baf72b9c8d5
{Natto133} <{tsukker1212@gmail.com}>
2022-08-08 21:49:22 +0900

    草機能の追加、ワロタ機能のワロタンゴタンバリンシャンシャン半島の周囲の装飾の間隔修正0.1.4

--- main.py
+++ main.py
@@ -12,19 +12,21 @@ client = discord.Client()
 
 # リプライ時の返答(実行テスト)
 async def reply(message):
-    reply = f'{message.author.mention}なんだい？ぼくを呼ぶなんて珍しいじゃないか。\nでも、忘れないで。ぼくは一生きみの友だちだよ！(v0.1.3)'
+    reply = f'{message.author.mention}なんだい？ぼくを呼ぶなんて珍しいじゃないか。\nでも、忘れないで。ぼくは一生きみの友だちだよ！(v0.1.4)'
     await message.channel.send(reply)
-
-@client.event
-async def on_message(message):
-    if client.user in message.mentions:
-        await reply(message)
 
 @client.event
 async def on_message(message):
     if message.author.bot:
         return
+
+    if client.user in message.mentions:
+        await reply(message)
+
     if message.content == "ワロタ" or message.content == "わろた":
-        await message.channel.send(f"{message.author.mention}\n＿人人人人人人人人人人人人人人人人人人人人＿\n＞　ワロタンゴタンバリンシャンシャン半島　＜\n￣Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y￣")
+        await message.channel.send(f"{message.author.mention}\n＿人人人人人人人人人人人人人人人人人人人人＿\n＞　ワロタンゴタンバリンシャンシャン半島　＜\n￣Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y￣")
+
+    if message.content == "w" or message.content == "ｗ" or message.content == "くさ" or message.content == "草":
+        await message.channel.send("地球温暖化対策に貢献できて偉い！\nでも草を生やすな")
 
 client.run(TOKEN)
